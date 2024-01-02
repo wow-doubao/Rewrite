231c0e90392a4c853d31ff862fcd395ccca1e482
王梓毅 <412760804@qq.com>
2024-01-02 15:48:38 +0800

    first commit

---
+++ m3u8/yjdm.js
@@ -0,0 +1,34 @@
+/*************************************
+
+项目名称：妖精动漫
+软件版本：1.0.0
+下载地址：https://is.gd/0LOaED
+脚本作者：南图
+使用声明：⚠️仅供参考，🈲️转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https:\/\/yjaoxapi\.cevt\.show.*\.m3u8 url script-request-header https://raw.githubusercontent.com/wow-doubao/Rewrite/main/m3u8/yjdm.js
+
+[mitm]
+hostname = yjaoxapi.cevt.show
+
+*************************************/
+const headers = $request.headers
+if (!headers.hasOwnProperty("X-Playback-Session-Id")) $done()
+
+const url = $request.url
+const newToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0aW1lc3RhbXAiOjE3MDM3MzQ4NDA4MzI4NzAwMDAsInVpZCI6MjI3MzA0NTh9.CCormhIlooOQJZyJq2qz_SdBKViiOww0Hib2Y-A-jY4";
+
+const updatedUrl = replaceTokenInUrl(url, newToken);
+console.log(`视频链接捕获成功:${updatedUrl}`)
+$notify("视频链接捕获成功", "点击此通知在线观看", "视频还没开始播放之前会通知1-4次", updatedUrl);
+
+function replaceTokenInUrl(url, newToken) {
+  let matchResult = url.match(/^(.*\.m3u8)/);
+  matchResult = matchResult ? matchResult[1] : "";
+
+  return `${matchResult}?token=${newToken}`
+}
+$done()
