304dda574c40fd17336c53009bfc556fa6d2e081
王梓毅 <412760804@qq.com>
2024-01-02 17:18:10 +0800

    add

--- m3u8/yjdm.js
+++ m3u8/yjdm.js
@@ -15,22 +15,22 @@
 hostname = yjaoxapi.cevt.show
 
 *************************************/
-const headers = $request.headers
-if (!headers.hasOwnProperty("X-Playback-Session-Id")) $done()
+// const headers = $request.headers
+// if (!headers.hasOwnProperty("X-Playback-Session-Id")) $done()
 
-const url = $request.url
-const newToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0aW1lc3RhbXAiOjE3MDM3MzQ4NDA4MzI4NzAwMDAsInVpZCI6MjI3MzA0NTh9.CCormhIlooOQJZyJq2qz_SdBKViiOww0Hib2Y-A-jY4";
+// const url = $request.url
+// const newToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0aW1lc3RhbXAiOjE3MDM3MzQ4NDA4MzI4NzAwMDAsInVpZCI6MjI3MzA0NTh9.CCormhIlooOQJZyJq2qz_SdBKViiOww0Hib2Y-A-jY4";
 
-function replaceTokenInUrl(url, newToken) {
-  let matchResult = url.match(/^(.*\.m3u8)/);
-  matchResult = matchResult ? matchResult[1] : "";
+// function replaceTokenInUrl(url, newToken) {
+//   let matchResult = url.match(/^(.*\.m3u8)/);
+//   matchResult = matchResult ? matchResult[1] : "";
 
-  return `${matchResult}?token=${newToken}`
-}
+//   return `${matchResult}?token=${newToken}`
+// }
 
-const updatedUrl = replaceTokenInUrl(url, newToken);
-$notify("视频链接捕获成功", "点击此通知在线观看", "视频还没开始播放之前会通知1-4次", updatedUrl);
-console.log(`视频链接捕获成功:${updatedUrl}`)
+// const updatedUrl = replaceTokenInUrl(url, newToken);
+// $notify("视频链接捕获成功", "点击此通知在线观看", "视频还没开始播放之前会通知1-4次", updatedUrl);
+// console.log(`视频链接捕获成功:${updatedUrl}`)
 
-$done()
-
+// $done()
+$notify("视频链接捕获成功", "点击此通知在线观看", "视频还没开始播放之前会通知1-4次", 'https://www.baidu.com/');
