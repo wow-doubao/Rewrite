9c2b2824bf992c9ddae0899449f66821dc022f9b
wzy <wzy412760804@outlook.com>
2024-01-23 08:13:38 +0800

    feat: add

--- m3u8/yjdm.js
+++ m3u8/yjdm.js
@@ -8,10 +8,10 @@
 **************************************
 
 [rewrite_local]
-^https:\/\/yjaoxapi\.cevt\.show.*\.m3u8 url script-request-header https://raw.githubusercontent.com/wow-doubao/Rewrite/main/m3u8/yjdm.js
+^https:\/\/is5x9\.cc.*\.m3u8 url script-request-header https://raw.githubusercontent.com/wow-doubao/Rewrite/main/m3u8/yjdm.js
 
 [mitm]
-hostname = yjaoxapi.cevt.show
+hostname = is5x9.cc
 
 *************************************/
 const headers = $request.headers
