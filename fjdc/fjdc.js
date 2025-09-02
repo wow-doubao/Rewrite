833901bcf32eab59b826f83018d794e9d3d35070
王梓毅 <412760804@qq.com>
2025-09-02 11:28:28 +0800

    feat: add

---
+++ fjdc/fjdc.js
@@ -0,0 +1,25 @@
+/*************************************
+
+项目名称：菲记单词
+软件版本：1.0.0
+脚本作者：南图
+使用声明：⚠️仅供参考，🈲️转载与售卖！
+
+**************************************
+
+[rewrite_local]
+^https:\/\/schoolapi\.xutongkeji\.com\/fjdc\/user\/home url script-response-body https://raw.githubusercontent.com/wow-doubao/Rewrite/main/fjdc/fjdc.js
+
+[mitm]
+hostname = schoolapi.xutongkeji.com
+
+*************************************/
+
+const obj = JSON.parse($response.body);
+
+chxm1023.data = {
+  ...obj.data,
+  "expireDate" : "2099-09-09",
+};
+
+$done({body : JSON.stringify(chxm1023)});
